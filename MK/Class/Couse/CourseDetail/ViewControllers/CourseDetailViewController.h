831a25b9715c9df160a6327ba179031e1b359081
zhouyang <245657589@qq.com>
2019-03-20 12:25:36 +0800

    【代码】测试新建文件夹

---
+++ MK/Class/Couse/CourseDetail/ViewControllers/CourseDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  CourseDetailViewController.h
+//  MK
+//
+//  Created by 周洋 on 2019/3/20.
+//  Copyright © 2019年 周洋. All rights reserved.
+//
+
+#import "MKBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CourseDetailViewController : MKBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
